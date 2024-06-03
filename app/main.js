09afed8130979b223febaf94bf87e7fbb624c62f
Asz <126867549+DadaLober@users.noreply.github.com>
2024-06-03 22:24:51 +0800

    pass stage

--- app/main.js
+++ app/main.js
@@ -10,9 +10,19 @@ const server = net.createServer((socket) => {
 		server.close();
 	});
 	socket.on("data", (data) => {
+		let responseStatus;
 		const path = data.toString().split(" ")[1];
-		const responseStatus = path === '/' ? "200 OK" : "404 Not Found";
+		const str = path.split("/")[2];
+		// console.log(path);
+		// console.log(str);
+		if (path === `/` || path === `/echo/${str}`) {
+			responseStatus = "200 OK";
+		} 
+		else {
+			responseStatus = "404 Not Found";
+		}
 		socket.write(`HTTP/1.1 ${responseStatus}\r\n\r\n`);
+		path === `/echo/${str}` ? socket.write(`Content-Type: text/plain\r\n\r\nContent-Length:${str.length}\r\n\r\n${str}`) : socket.write(`Content-Type: text/plain\r\n\r\nContent-Length: 0\r\n\r\n`);
 	});
 });
 
