1f5747b71a054229ed87213a433b2fb63afa4ab3
Asz <126867549+DadaLober@users.noreply.github.com>
2024-06-04 22:33:07 +0800

    read header implementation

--- app/main.js
+++ app/main.js
@@ -1,3 +1,4 @@
+const { Agent } = require("http");
 const net = require("net");
 
 console.log("Logs...");
@@ -5,12 +6,13 @@ console.log("Logs...");
 const RESPONSE_OK = "200 OK";
 const RESPONSE_NOT_FOUND = "404 Not Found";
 const CONTENT_TYPE = "text/plain";
+const header = {};
 
 
-function buildResponse(path, str) {
+function buildResponse() {
 
-	if (path === "/" || path.includes("/echo/")) {
-		return `HTTP/1.1 ${RESPONSE_OK}\r\nContent-Type: ${CONTENT_TYPE}\r\nContent-Length: ${str.length}\r\n\r\n${str}`;
+	if (header["GET"] === "/" || header["GET"].includes("/user-agent")) {
+		return `HTTP/1.1 ${RESPONSE_OK}\r\nContent-Type: ${CONTENT_TYPE}\r\nContent-Length: ${header["User-Agent"].length}\r\n\r\n${header["User-Agent"]}`;
 	} else {
 		return `HTTP/1.1 ${RESPONSE_NOT_FOUND}\r\n\r\n`;
 	}
@@ -22,10 +24,13 @@ const server = net.createServer((socket) => {
 		server.close();
 	});
 	socket.on("data", (data) => {
-		const path = data.toString().split(" ")[1];
-		const str = path.split("/echo/")[1];
-
-		socket.write(buildResponse(path, str));
+		const path = data.toString().split("\r\n").filter(e => e);
+		for (const i of path) {
+			const [key, value] = i.split(" ");
+			header[key.replace(":", "")] = value;
+		}
+		// console.log(header);
+		socket.write(buildResponse());
 	});
 });
 
